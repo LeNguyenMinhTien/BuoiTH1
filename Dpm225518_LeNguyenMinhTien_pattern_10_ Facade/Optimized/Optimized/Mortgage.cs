0dcc369521e92170cbfb86208b3d0308d94322d5
LeNguyenMinhTien <tien_dpm225518@student.agu.edu.vn>
2025-09-22 18:28:02 +0700

    Add files via upload

---
+++ Dpm225518_LeNguyenMinhTien_pattern_10_ Facade/Optimized/Optimized/Mortgage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Optimized
+{
+    public class Mortgage
+    {
+        private readonly Bank bank = new Bank();
+        private readonly Loan loan = new Loan();
+        private readonly Credit credit = new Credit();
+
+        public bool IsEligible(Customer cust, int amount)
+        {
+            Console.WriteLine("{0} applies for {1:C} loan\n",
+                cust.Name, amount);
+
+            bool eligible = true;
+
+            if (!bank.HasSufficientSavings(cust, amount))
+            {
+                eligible = false;
+            }
+            else if (!loan.HasNoBadLoans(cust))
+            {
+                eligible = false;
+            }
+            else if (!credit.HasGoodCredit(cust))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+    }
+}
---
+++ Dpm225518_LeNguyenMinhTien_pattern_10_ Facade/Optimized/Optimized/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Optimized
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Facade
+            var mortgage = new Mortgage();
+
+            // Evaluate mortgage eligibility for customer
+            var customer = new Customer("Ann McKinsey");
+            bool eligible = mortgage.IsEligible(customer, 125000);
+
+            string result = eligible ? "Approved" : "Rejected";
+            WriteLine($"\n{customer.Name} has been {result}");
+
+            // Wait for user
+            ReadKey();
+        }
+    }
+}
