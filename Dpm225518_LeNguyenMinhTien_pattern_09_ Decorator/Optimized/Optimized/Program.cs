a7526215a81709ad531996f94b0d31b58b4aeb7a
LeNguyenMinhTien <tien_dpm225518@student.agu.edu.vn>
2025-09-22 18:25:56 +0700

    Add files via upload

---
+++ Dpm225518_LeNguyenMinhTien_pattern_09_ Decorator/Optimized/Optimized/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimized
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create book
+            var book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            // Create video
+            var video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            // Make video borrowable, then borrow and display
+            Console.WriteLine("\nMaking video borrowable:");
+            var borrow = new Borrowable<Video>(video);
+            borrow.BorrowItem("Customer #1");
+            borrow.BorrowItem("Customer #2");
+            borrow.Display();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dpm225518_LeNguyenMinhTien_pattern_09_ Decorator/Optimized/Optimized/Video.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optimized
+{
+    public class Video : LibraryItem<Video>
+    {
+        private readonly string director;
+        private readonly string title;
+        private readonly int playTime;
+
+        public Video(string director, string title, int numCopies, int playTime)
+        {
+            this.director = director;
+            this.title = title;
+            NumCopies = numCopies;
+            this.playTime = playTime;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\nVideo ----- ");
+            Console.WriteLine($" Director: {director}");
+            Console.WriteLine($" Title: {title}");
+            Console.WriteLine($" # Copies: {NumCopies}");
+            Console.WriteLine($" Playtime: {playTime}\n");
+        }
+
+    }
+}
