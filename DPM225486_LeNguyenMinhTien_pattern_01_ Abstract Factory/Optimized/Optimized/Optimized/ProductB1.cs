ed69854cbc767791e18b1e423ef7f565a9f1e61b
LeNguyenMinhTien <tien_dpm225518@student.agu.edu.vn>
2025-09-22 18:17:12 +0700

    Add files via upload

---
+++ DPM225486_LeNguyenMinhTien_pattern_01_ Abstract Factory/Optimized/Optimized/Optimized/ProductB1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Optimized
+{
+    public class Lion : ICarnivore
+    {
+        // Eat Wildebeest
+        public void Eat(IHerbivore h) =>
+            Console.WriteLine($"{GetType().Name} eats {h.GetType().Name}");
+    }
+}
---
+++ DPM225486_LeNguyenMinhTien_pattern_01_ Abstract Factory/Optimized/Optimized/Optimized/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Optimized
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create and run the African animal world
+            var africa = new AnimalWorld<Africa>();
+            africa.RunFoodChain();
+
+            // Create and run the American animal world
+            var america = new AnimalWorld<America>();
+            america.RunFoodChain();
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
